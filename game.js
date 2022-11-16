d4ab5c2a0f6a47bb1f5c811692b5c07cc0dd9820
Tim2197 <79378803+Tim2197@users.noreply.github.com>
2022-11-16 11:50:19 -0600

    Adding initial main function in game.js

---
+++ game.js
@@ -0,0 +1,13 @@
+// Load from local storage here
+
+
+// Take save from localstorage and populate player here
+
+
+// Initial game state, main function runs all of the games functionality
+function main(player) {
+    // Loop that makes the game run until end game is initiated by player from menu
+    // Menu has shut off button that will turn gameOn to false
+    while(gameOn) {
+    }
+}
