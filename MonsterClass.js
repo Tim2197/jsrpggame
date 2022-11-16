4d22594f66412901b747eae6b152a54d3a212ff9
BuildTools <unconfigured@null.spigotmc.org>
2022-11-16 13:08:07 -0600

    Added MonsterClass.js + added death check + Dmg

---
+++ MonsterClass.js
@@ -0,0 +1,174 @@
+class Monster {
+    health = 10;
+    strength = 1;
+    agility = 4;
+    isKillable = true;
+    dropsLoot = true;
+    dropsExp = true;
+    expDropped = 10;
+    lootTable = [
+        {
+            name: "Zarthos' incredible Ocarina",
+            probability: "rare"
+        },
+        {
+             name: "Aiko's Handsome Ring",
+             probability: "mythic"
+        },
+        {   name: "Kanuna's Amulet of Truth",
+            probability: "uncommon"
+        }];
+    armorClass = 5;
+    monsterType = 'goblin';
+    isDead = false;
+
+    get properties() {
+        return [
+            'health',
+            'strength',
+            'agility',
+            'isKillable',
+            'dropsLoot',
+            'givesExp',
+            'expDropped',
+            'lootTable',
+            'armorClass',
+            'monsterType'
+        ]
+    }
+
+
+    constructor(health, strength, agility, expDropped, lootTable, armorClass, monsterType){
+        this.health = health;
+        this.strength = strength;
+        this.agility = agility;
+        this.isKillable;
+        this.dropsLoot;
+        this.dropsExp;
+        this.expDropped = expDropped;
+        this.lootTable = lootTable;
+        this.armorClass = armorClass;
+        this.monsterType = monsterType;
+        this.isDead;
+    }
+
+    attack(type){
+        if(type == 'basic'){
+            console.log(`${this.monsterType} does a basic attack! ${this.monsterType} does ${this.strength} damage to the player!`)
+        }
+        if(type == 'special'){
+            console.log(`${this.monsterType} does a basic attack! ${this.monsterType} does ${this.strength * 1.5} damage to the player!`)
+        }
+        if(type == 'final'){
+            console.log(`${this.monsterType} does a basic attack! ${this.monsterType} does ${this.strength * 2} damage to the player!`)
+        }
+    }
+
+    defend() {
+        console.log(`${this.monsterType} tries defending`)
+    }
+
+    dodge() {
+        if (this.agility > Player.agility){
+            console.log(`${this.monsterType} is able to dodge.`)
+        }
+    }
+
+    hurt(dmg) {
+        if(dmg){
+            this.health = this.health - dmg;
+            this.checkDeath()
+        }
+    }
+
+    heal() {
+        console.log(`${this.monsterType} heals itself for ${Math.floor(Math.random() * this.health)}`)
+    }
+
+    loot() {
+
+        function lootDropChance() {
+            return Math.floor(Math.random() * 99)
+        }
+
+        let rarities = {
+            always: 100,
+            common: 90,
+            uncommon: 70,
+            rare: 50,
+            mythic: 10,
+            legendary: 1,
+        };
+
+        console.log("\n")
+
+        for (let i = 0; i < this.lootTable.length; i++){
+            if(this.lootTable[i].probability == "always"){
+                console.log(`Player received ${this.lootTable[i].name}`)
+            }
+            if(this.lootTable[i].probability == "common" && lootDropChance() < rarities.common){
+                console.log(`Player received ${this.lootTable[i].name}`)
+            }
+            if(this.lootTable[i].probability == "uncommon" && lootDropChance() < rarities.uncommon){
+                console.log(`Player received ${this.lootTable[i].name}`)
+            }
+            if(this.lootTable[i].probability == "rare" && lootDropChance() < rarities.rare){
+                console.log(`Player received ${this.lootTable[i].name}`)
+            }
+            if(this.lootTable[i].probability == "mythic" && lootDropChance() < rarities.mythic){
+                console.log(`Player received ${this.lootTable[i].name}`)
+            }
+            if(this.lootTable[i].probability == "legendary" && lootDropChance() < rarities.legendary){
+                console.log(`Player received ${this.lootTable[i].name}`)
+            }
+        }
+
+        console.log("\n")
+    }
+
+    checkDeath() {
+        if(this.health <= 0){
+            this.setDeath()
+        }
+    }
+
+    setDeath() {
+        let dead;
+        this.isDead = true;
+        if(this.isDead == true){
+            dead = 'dead'
+        }
+        console.log(`${this.monsterType} is ${dead}`);
+    }
+
+    
+
+}
+// These Two Were Tests From before there was a randomized loot pool that required the loot table to be an object
+// let goblin = new Monster(10, 20, 1, 1000, ["Zarthos' Galactic Ocarina"], 12, 'goblin');
+// let wraith = new Monster(10, 25, 2, 1250, ["Kavall's Hammer", "Healing Potion", "Wraith Essense", "Wisp in a Bottle"], 10, 'wraith');
+let lootMonsterTest = new Monster(10, 25, 2, 1500, [
+    {
+        name: "Zarthos' incredible Ocarina",
+        probability: "rare"
+    },
+    {
+         name: "Aiko's Handsome Ring",
+         probability: "mythic"
+    },
+    {   name: "Kanuna's Amulet of Truth",
+        probability: "uncommon"
+    },
+    {
+        name: "lootMonsterTest drop",
+        probability: "always"
+    }], 10, 'lootMonsterTest')
+
+console.log(lootMonsterTest.isDead)
+console.log(lootMonsterTest.health)
+lootMonsterTest.hurt(9)
+console.log(lootMonsterTest.health)
+console.log(lootMonsterTest.isDead)
+lootMonsterTest.checkDeath()
+lootMonsterTest.hurt(1)
+console.log(lootMonsterTest.isDead)
